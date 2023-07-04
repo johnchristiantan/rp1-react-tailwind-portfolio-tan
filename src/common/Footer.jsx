996dca56b8222bcf48390133b0a60613811f64f3
John Christian Tan <jchristian.abante@gmail.com>
2023-07-04 20:04:14 +0800

    Fixed footer

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='absolute bottom-0 w-full py-3 text-center bg-slate-50'>&copy; Copyright</div>
+    <div className='fixed bottom-0 w-full py-3 text-center bg-slate-50'>&copy; Copyright</div>
   )
 }
 
